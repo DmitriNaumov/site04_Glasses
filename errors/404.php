6a7b6d9e760e2e7367e39b133121c48187720ef2
DmitriNaumov <dmitr9628@gmail.com>
2022-05-21 14:35:49 +0300

    Ready project
    Ready project

---
+++ errors/404.php
@@ -0,0 +1,174 @@
+<?php
+header("HTTP/1.1 404 Not Found");
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>404</title>
+	<link rel="icon" href="../img/icon.jpg" type="icon/x-icon">
+	<link rel="stylesheet" href="../style/normalize.css">
+	<link rel="stylesheet" href="../style/slick.css">
+	<link rel="stylesheet" href="../style/slick-theme.css">
+	<link rel="stylesheet" href="../style/fonts.css">
+	<link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+
+<!-- div .preloader -->
+<div class="preloader">
+	<div class="preloader__wrapp">
+		<div class="preloader__txt">
+			<p>loading...</p>
+		</div>
+		<div class="preloader__line"></div>
+	</div>
+</div>
+<!-- /div .preloader END -->
+
+<!-- div .wrapp -->
+<div class="wrapp _bg-color_general">
+	
+	<!-- nav .nav -->
+	<nav class="nav">
+		
+		<!-- div .content -->
+		<div class="content">
+			
+			<!-- div .nav__wrapp -->
+			<div class="nav__wrapp">
+				
+				<!-- div .nav__logo -->
+				<a href="../index.html" class="nav__logo">
+					<img src="../img/logo.png" alt="logo">
+				</a>
+				<!-- /div .nav__logo END -->
+				
+				<!-- div .nav__menu -->
+				<div class="nav__menu">
+					<a href="../index.html" class="nav__portfolio">главная</a>
+					<a href="#" class="nav__burger"></a>
+				</div>
+				<!-- /div .nav__menu END -->
+			</div>
+			<!-- /div .nav__wrapp END -->
+		</div>
+		<!-- /div .content END -->
+	</nav>
+	<!-- /nav .nav END -->
+	
+	<!-- header .header__about -->
+	<header class="header header__not">
+		
+		<div class="header__page-shadow"></div>
+		
+		<!-- div .header__title -->
+		<div class="header__page-title">
+			<h1 class="header__page-h1">404</h1>
+		</div>
+		<!-- /div .header__title END -->
+		
+		<!-- div .header__bottom -->
+		<div class="header__bottom">
+			
+			<!-- div .content -->
+			<div class="content">
+				
+				<!-- div .header__bottom-wrapp -->
+				<div class="header__bottom-wrapp">
+				
+					<div class="header__social-wrapp">
+						
+						<ul class="header__social">
+							<li class="header__social-item"><a href="https://www.pinterest.cl/dmitr9628/ndwebdev/" class="header__social-link">pi</a></li>
+							<li class="header__social-item"><a href="https://vk.com/nd_webdev" class="header__social-link">vk</a></li>
+							<li class="header__social-item"><a href="https://www.instagram.com/nd.webdev/" class="header__social-link">i</a></li>
+						</ul>
+						
+					</div>
+				</div>
+				<!-- /div .header__bottom-wrapp END -->
+			</div>
+			<!-- /div .content END -->
+		</div>
+		<!-- /div .header__bottom END -->
+	</header>
+	<!-- /header .header__about END -->
+	
+	<!-- div .popap-menu -->
+	<div class="popap-menu">
+
+		<div class="popap-wrapp">
+			
+			<div class="popa-menu__close">
+				<div class="bl-img bl-1">
+					<svg enable-background="new 0 0 96 96" version="1.1" viewBox="0 0 96 96" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M24,26.129L60.451,48L24,69.87V26.129 M16,12v72l60-36L16,12L16,12z"></path></svg>
+				</div>
+				<div class="bl-img bl-2">
+					<svg enable-background="new 0 0 96 96" version="1.1" viewBox="0 0 96 96" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M24,26.129L60.451,48L24,69.87V26.129 M16,12v72l60-36L16,12L16,12z"></path></svg>
+				</div>
+				<div class="bl-img bl-3">
+					<svg enable-background="new 0 0 96 96" version="1.1" viewBox="0 0 96 96" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M24,26.129L60.451,48L24,69.87V26.129 M16,12v72l60-36L16,12L16,12z"></path></svg>
+				</div>
+			</div>
+			
+			<div class="popap-menu__fone">
+				<span>menu</span>
+			</div>
+			
+			<div class="popap-menu__nav">
+
+				<div class="popap-menu__wrapp">
+					<a class="popap-menu__link" href="../index.html">
+						<span class="popap-menu__hover hover_gradient"></span>
+						<span class="link-name">главная</span>
+					</a>
+					<a class="popap-menu__link" href="../portfolio.html">
+						<span class="popap-menu__hover hover_gradient"></span>
+						<span class="link-name">портфолио</span>
+					</a>
+					<a class="popap-menu__link active" href="../about.html">
+						<span class="popap-menu__hover hover_gradient"></span>
+						<span class="link-name">обо мне</span>
+					</a>
+					<a class="popap-menu__link" href="../contacts.html">
+						<span class="popap-menu__hover hover_gradient"></span>
+						<span class="link-name">контакты</span>
+					</a>
+				</div>
+
+				<div class="popap-menu__social">
+					<ul class="popap-menu__social-wrapp">
+						<li class="popap-menu__item">
+							<span class="popap-menu__social-hover hover_gradient"></span>
+							<a href="https://www.pinterest.cl/dmitr9628/ndwebdev/" class="popap-menu__social-link">pi</a>
+						</li>
+						<li class="popap-menu__item">
+							<span class="popap-menu__social-hover hover_gradient"></span>
+							<a href="https://vk.com/nd_webdev" class="popap-menu__social-link">vk</a>
+						</li>
+						<li class="popap-menu__item">
+							<span class="popap-menu__social-hover hover_gradient"></span>
+							<a href="https://www.instagram.com/nd.webdev/" class="popap-menu__social-link">i</a>
+						</li>
+					</ul>
+				</div>
+				
+			</div>
+
+		</div>
+
+	</div>
+	<!-- /div .popap-menu -->
+</div>
+<!-- /div .wrapp END -->
+
+<!-- script -->
+<script src="../js/jquery-3.1.1.min.js"></script>
+<script src="../js/slick.min.js"></script>
+<script src="../js/js.js"></script>
+<!-- script END -->
+</body>
+</html>
